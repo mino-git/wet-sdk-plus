0b299cb0415eb0637ccf6d2e240889f70ae8bcfb
mino <n.mitschke@mail.de>
2012-05-10 18:46:20 +0000

    batch scripts configuration

--- src/Release/acqu.bat
+++ src/Release/acqu.bat
@@ -5,7 +5,4 @@ rename z.zip z.pk3
 copy z.pk3 C:\Users\akk\wetdev1\z
 copy qagame_mp_x86.dll C:\Users\akk\wetdev1\z
 
-copy z.pk3 C:\Users\akk\wetdev2\z
-copy qagame_mp_x86.dll C:\Users\akk\wetdev2\z
-
 del z.pk3
