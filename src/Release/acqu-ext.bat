37da9abc0ecd4ed414648aeaa932f1e037992448
mino <n.mitschke@mail.de>
2012-05-10 18:39:37 +0000

    added release folder and 2 windows batch scripts for faster setting up the mod (zip files, rename, copy etc.)

---
+++ src/Release/acqu-ext.bat
@@ -0,0 +1,11 @@
+7z.exe u -tzip z.zip cgame_mp_x86.dll ui_mp_x86.dll -r
+
+rename z.zip z.pk3
+
+copy z.pk3 C:\Users\akk\wetdev1\z
+copy qagame_mp_x86.dll C:\Users\akk\wetdev1\z
+
+copy z.pk3 C:\Users\akk\wetdev2\z
+copy qagame_mp_x86.dll C:\Users\akk\wetdev2\z
+
+del z.pk3
