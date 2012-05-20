87092f47f0bd646b06152a4460af7209816cc330
mino <n.mitschke@mail.de>
2012-05-20 02:01:27 +0000

    update batchscripts

--- src/Release/acqu-ext.bat
+++ src/Release/acqu-ext.bat
@@ -9,7 +9,7 @@ copy ui_mp_x86.dll C:\Users\akk\wetdev1\z
 
 copy z.pk3 C:\Users\akk\wetdev2\z
 copy cgame_mp_x86.dll C:\Users\akk\wetdev2\z
-copy qagame_mp_x86.dll C:\Users\akk\wetdev1\z
-copy ui_mp_x86.dll C:\Users\akk\wetdev1\z
+copy qagame_mp_x86.dll C:\Users\akk\wetdev2\z
+copy ui_mp_x86.dll C:\Users\akk\wetdev2\z
 
 del z.pk3
