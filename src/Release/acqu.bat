defee0e6507cf35054c4f4c41c182bfe5589a3ab
mino <n.mitschke@mail.de>
2012-05-20 01:59:37 +0000

    update batchscripts

--- src/Release/acqu.bat
+++ src/Release/acqu.bat
@@ -3,6 +3,8 @@
 rename z.zip z.pk3
 
 copy z.pk3 C:\Users\akk\wetdev1\z
+copy cgame_mp_x86.dll C:\Users\akk\wetdev1\z
 copy qagame_mp_x86.dll C:\Users\akk\wetdev1\z
+copy ui_mp_x86.dll C:\Users\akk\wetdev1\z
 
 del z.pk3
