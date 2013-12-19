eb6b679d5dca9d6c61058be1215deb517bb85167
mino <n.mitschke@mail.de>
2013-12-19 15:24:26 +0100

    More restructuring (windows)

--- src/build/install-ext.bat
+++ src/build/install-ext.bat
@@ -1,8 +1,12 @@
 REM install-ext.bat - this installs the mod into two destination directories
 
-set WETDIR1=C:\Users\herb28\WETc
-set WETDIR2=C:\Users\herb28\WETs
-set MODNAME=z
+REM -- CONFIG START
+
+set WETDIR1=C:\Users\Random\WETc\
+set WETDIR2=C:\Users\Random\WETs\
+set MODNAME=mymod
+
+REM -- CONFIG END
 
 7z.exe u -tzip z.zip cgame_mp_x86.dll ui_mp_x86.dll gfx scripts sound -r
 
@@ -17,11 +21,5 @@ copy z.pk3 %WETDIR2%\%MODNAME%
 copy cgame_mp_x86.dll %WETDIR2%\%MODNAME%
 copy qagame_mp_x86.dll %WETDIR2%\%MODNAME%
 copy ui_mp_x86.dll %WETDIR2%\%MODNAME%
-
-REM lua lib - install only to server dir
-copy lua5.1.dll %WETDIR2%
-
-REM sqlite lib - install only to server dir
-copy sqlite3.dll %WETDIR2%
 
 del z.pk3
--- src/build/install.bat
+++ src/build/install.bat
@@ -1,7 +1,11 @@
 REM install.bat - this installs the mod into one destination directory
 
-set WETDIR1=C:\Users\akku\WETc
-set MODNAME=z
+REM -- CONFIG START
+
+set WETDIR1=C:\Users\Random\WETc\
+set MODNAME=mymod
+
+REM -- CONFIG END
 
 7z.exe u -tzip z.zip cgame_mp_x86.dll ui_mp_x86.dll gfx scripts sound -r
 
@@ -11,8 +15,5 @@ copy z.pk3 %WETDIR1%\%MODNAME%
 copy cgame_mp_x86.dll %WETDIR1%\%MODNAME%
 copy qagame_mp_x86.dll %WETDIR1%\%MODNAME%
 copy ui_mp_x86.dll %WETDIR1%\%MODNAME%
-
-copy lua5.1.dll %WETDIR1%
-copy sqlite3.dll %WETDIR1%
 
 del z.pk3
