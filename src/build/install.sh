560547a70fae3ac2e4884ad8db0e9ea2ea5ad909
mino <n.mitschke@mail.de>
2013-12-23 12:28:27 +0100

    Update Linux install scripts.

--- src/build/install.sh
+++ src/build/install.sh
@@ -3,16 +3,16 @@
 # install.sh
 # this installs the mod into one destination directory
 
-WETDIR1='/home/linux/.etwolf/'
-MODNAME='z'
+ET_HOMEPATH='/home/random/.etwolf/'
+MODNAME='mymod'
 
 strip -s *.so
 
 zip -r temp cgame.mp.i386.so qagame.mp.i386.so ui.mp.i386.so gfx scripts sound
 
-mv ./temp.zip $WETDIR1/$MODNAME/z.pk3
-cp ./cgame.mp.i386.so $WETDIR1/$MODNAME/cgame.mp.i386.so
-cp ./qagame.mp.i386.so $WETDIR1/$MODNAME/qagame.mp.i386.so
-cp ./ui.mp.i386.so $WETDIR1/$MODNAME/ui.mp.i386.so
+mv ./temp.zip $ET_HOMEPATH/$MODNAME/z.pk3
+cp ./cgame.mp.i386.so $ET_HOMEPATH/$MODNAME/cgame.mp.i386.so
+cp ./qagame.mp.i386.so $ET_HOMEPATH/$MODNAME/qagame.mp.i386.so
+cp ./ui.mp.i386.so $ET_HOMEPATH/$MODNAME/ui.mp.i386.so
 
 echo "Done!"
