3d00175314ba5f1da2245f0766ca96da850d1e57
mino <n.mitschke@mail.de>
2013-12-23 14:57:02 +0100

    Update CompileInstall.html + install-ext.sh

--- src/build/install-ext.sh
+++ src/build/install-ext.sh
@@ -3,22 +3,22 @@
 # install-ext.sh
 # this installs the mod into two destination directories
 
-WETDIR1='/home/linux/.etwolf/'
-WETDIR2='/home/linux/.etwolf2/'
-MODNAME='z'
+ET_HOMEPATH1='/home/random/.etwolf/'
+ET_HOMEPATH2='/home/random/.etwolf2/'
+MODNAME='mymod'
 
 strip -s *.so
 
 zip -r temp cgame.mp.i386.so qagame.mp.i386.so ui.mp.i386.so gfx scripts sound
 
-cp ./temp.zip $WETDIR1/$MODNAME/z.pk3
-cp ./cgame.mp.i386.so $WETDIR1/$MODNAME/cgame.mp.i386.so
-cp ./qagame.mp.i386.so $WETDIR1/$MODNAME/qagame.mp.i386.so
-cp ./ui.mp.i386.so $WETDIR1/$MODNAME/ui.mp.i386.so
+cp ./temp.zip $ET_HOMEPATH1/$MODNAME/z.pk3
+cp ./cgame.mp.i386.so $ET_HOMEPATH1/$MODNAME/cgame.mp.i386.so
+cp ./qagame.mp.i386.so $ET_HOMEPATH1/$MODNAME/qagame.mp.i386.so
+cp ./ui.mp.i386.so $ET_HOMEPATH1/$MODNAME/ui.mp.i386.so
 
-mv ./temp.zip $WETDIR2/$MODNAME/z.pk3
-cp ./cgame.mp.i386.so $WETDIR2/$MODNAME/cgame.mp.i386.so
-cp ./qagame.mp.i386.so $WETDIR2/$MODNAME/qagame.mp.i386.so
-cp ./ui.mp.i386.so $WETDIR2/$MODNAME/ui.mp.i386.so
+mv ./temp.zip $ET_HOMEPATH2/$MODNAME/z.pk3
+cp ./cgame.mp.i386.so $ET_HOMEPATH2/$MODNAME/cgame.mp.i386.so
+cp ./qagame.mp.i386.so $ET_HOMEPATH2/$MODNAME/qagame.mp.i386.so
+cp ./ui.mp.i386.so $ET_HOMEPATH2/$MODNAME/ui.mp.i386.so
 
 echo "Done!"
